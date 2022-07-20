2465cd2a517972a619489177b5d6146db39ba9d7
Henri Forss <hforss@gmail.com>
2022-07-20 11:13:43 +0300

    linear colorScale

--- script.js
+++ script.js
@@ -19,39 +19,52 @@ const path = d3.geoPath(projection)
 /* Create g-element for paths. */
 const g = svg.append("g")
 
+/* Create colorScale. */
+const colorScale = d3.scaleLinear()
+  .domain([-0.05, 0, 0.05])
+  .range(["red", "white", "blue"])
+
 /* Define createMap(). */
 async function createMap() {
   const data = await d3.json("kunnat-clipped.geojson")
   const municipalities = data.features
 
-  const population = await d3.json("kunnat-vaestonmuutos.json")
+  const population = await d3.json("kunnat-vaestonmuutos-2.json")
 
-  /* Create a lookuptable. Using reduce() would be fancier. */
+  /* Create a lookup table. Using reduce() would be fancier. */
   populationTable = {}
   population.data.forEach((d) => {
     const key = parseInt(d.key[1].slice(2))
-    const value = parseInt(d.values[0])
-    populationTable[key] = value
+    const value1 = parseInt(d.values[0]) // absolute change
+    const value2 = parseInt(d.values[1]) // absolute population
+    const value3 = value1 / value2 // percentage change
+
+    /* Create entries for populationTable. */
+    populationTable[key] = {
+      popchange: value1,
+      popsize: value2,
+      popchangepercent: value3,
+    }
   })
 
+  /* Assign values in populationTable to municipalities. No need to look up data when all data is in same place. */
+  municipalities.forEach(d => {
+    Object.assign(d.properties, populationTable[parseInt(d.properties.natcode)])
+  })
+  
+  /* Draw paths. */
   g.selectAll("path")
     .data(municipalities)
     .enter()
     .append("path")
     .attr("id", (d) => d.properties.natcode)
-    .attr("fill", (d) => {
-      if (populationTable[parseInt(d.properties.natcode)] >= 0) {
-        return "lightgreen"
-      } else {
-        return "pink"
-      }
-    })
+    .attr("fill", d => colorScale(d.properties.popchangepercent))
     .attr("class", "municipality")
     .attr("name", (d) => d.properties.namefin)
     .attr("d", path)
     .on("click", showData)
     .append("title")
-    .text((d) => d.properties.namefin)
+    .text((d) => d.properties.namefin)  
 }
 
 /* Define showData() */
@@ -69,7 +82,11 @@ function showData() {
 
   d3.select(".div")
     .append("p")
-    .text(`Väestönlisäys: ${populationTable[municipalityNumber]}`)
+    .text(`Väestönlisäys: ${populationTable[municipalityNumber].popchange}`)
+
+  d3.select(".div")
+    .append("p")
+    .text(`Muutos: ${parseFloat(populationTable[municipalityNumber].popchangepercent * 100).toFixed(2)} %`)
 }
 
 /* Create div-element. */
