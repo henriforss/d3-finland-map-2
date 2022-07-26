966ddfb8dff05356636d30c4a4d76945df959966
Henri Forss <hforss@gmail.com>
2022-07-26 12:17:37 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-/* Define mapSelecter for selecting maps. */
-let mapSelector = 1
+/* Variable to choose map. */
+let mapSelector = "population"
+
+/* Create dropdown menu to select map. */
+const dropdownNode = document.getElementById("dropdown")
+const dropdownNodeElement = document.createElement("select")
+dropdownNode.appendChild(dropdownNodeElement)
+dropdownNodeElement.innerHTML =
+  "<option selected='selected' value='population'>Väestönkehitys</option><option value='economy'>Talous</option><option value='healthcare'>Terveydenhoitokustannukset</option>"
+
+/* Dropdown event listener. */
+dropdownNodeElement.addEventListener("change", handleSelect)
+
+/* Dropdown event handler. */
+function handleSelect(event) {
+  mapSelector = event.target.value
+  createMap(mapSelector)
+}
+
+/* Define projection. */
+const projection = d3.geoMercator()
+  .scale(1200)
+  .translate([-350, 2100])
+
+/* Create path. */
+const path = d3.geoPath(projection)
 
 /* Define svg-size. */
 const width = 350
@@ -11,14 +35,6 @@ const svg = d3.select("#map")
   .attr("width", width)
   .attr("height", height)
 
-/* Define projection. */
-const projection = d3.geoMercator()
-  .scale(1200)
-  .translate([-350, 2100])
-
-/* Create path. */
-const path = d3.geoPath(projection)
-
 /* Create background-rect. */
 const background = svg.append("rect")
   .attr("width", width)
@@ -28,22 +44,6 @@ const background = svg.append("rect")
 
 /* Create g-element for paths in map. */
 const map = svg.append("g")
-
-
-
-
-/* Create colorScale1 (population). */
-const colorScale1 = d3.scaleLinear()
-  .domain([-0.05, 0, 0.05])
-  .range(["red", "white", "blue"])
-
-/* Create colorScale3 (healthcare). */
-const colorScale3 = d3.scaleLinear()
-  .domain([2400, 6800])
-  .range(["yellow", "purple"])
-
-  
-
 
 /* Create background-element for legend. */
 const legendBackgound = svg.append("rect")
@@ -55,99 +55,262 @@ const legendBackgound = svg.append("rect")
 /* Create a group-element for the legend. */
 const legend = svg.append("g")
 
-/* Create defs-element for linearGradient. */
-const defs = legend.append("defs")
+/* Define createMap(). */
+async function createMap(selectedmap) {
 
-/* Create linearGradient. This is the direction of the gradient. */
-const linearGradient = defs.append("linearGradient")
-  .attr("id", "linear-gradient")
-  .attr("x1", "0%")
-  .attr("y1", "0%")
-  .attr("x2", "100%")
-  .attr("y2", "0%")
+  /* Create population map. */
+  if (selectedmap == "population") {
+    console.log("popu")
 
-/* Create "stops" from colorScale. */
-linearGradient.selectAll("stop")
-  .data(colorScale1.range())
-  .enter()
-  .append("stop")
-  .attr("offset", (d, i) => {
-    return i / (colorScale1.range().length - 1)
-  })
-  .attr("stop-color", d => d)
+    /* Define colorScale. Every map has own colorScale. */
+    const colorScale = d3.scaleLinear()
+      .domain([-0.05, 0, 0.05])
+      .range(["red", "white", "green"])
+      
+    /* Remove defs element from legend before creating new one. */  
+    legend.selectAll("defs")
+      .remove()
 
-/* Create the rect that contains the gradient, referenced by id. */
-legend.append("rect")
-  .attr("width", 300)
-  .attr("height", 10)
-  .style("fill", "url(#linear-gradient)")
-  .attr("transform", "translate(25, 560)")
+    /* Create defs-element for linearGradient. */
+    const defs = legend.append("defs")
 
-// /* Create some lines on the gradient. */
-// legend.selectAll("line")
-//   .data([0, 1, 2, 3, 4])
-//   .enter()
-//   .append("line")
-//   .attr("x1", 0)
-//   .attr("y1", 0)
-//   .attr("x2", 0)
-//   .attr("y2", 10)
-//   .style("stroke", "black")
-//   .style("stroke-width", 0.25)
-//   .attr("transform", d => {
-//     const start = 25
-//     const x = 75
-//     return `translate(${d * x + start}, 560)`
-//   })
+    /* Create linearGradient. This is the direction of the gradient. */
+    const linearGradient = defs.append("linearGradient")
+      .attr("id", "linear-gradient")
+      .attr("x1", "0%")
+      .attr("y1", "0%")
+      .attr("x2", "100%")
+      .attr("y2", "0%")
+  
+    /* Create "stops" from colorScale. */
+    linearGradient.selectAll("stop")
+      .data(colorScale.range())
+      .enter()
+      .append("stop")
+      .attr("offset", (d, i) => {
+        return i / (colorScale.range().length - 1)
+      })
+      .attr("stop-color", d => d)
+  
+    /* Remove rect-elements from legend before creating new one. */
+    legend.selectAll("rect")
+      .remove()
 
-/* Append some text to the gradient. */
-legend.selectAll("text")
-  .data(["-5%", "-2.5%", "0%", "2.5%", "5%"])
-  .enter()
-  .append("text")
-  .attr("y", 585)
-  .attr("x", (d, i) => {
+    /* Create the rect that contains the gradient, referenced by id. */
+    legend.append("rect")
+      .attr("width", 300)
+      .attr("height", 10)
+      .style("fill", "url(#linear-gradient)")
+      .attr("transform", "translate(25, 560)")
+  
+    /* Remove all text-elements from legend before creating new ones. */
+    legend.selectAll("text")
+      .remove()
 
-    return `${(i) * 71 + 25}`
-  })
-  .text(d => d)
-  .attr("font-size", "0.7rem")
-  .attr("font-family", "sans-serif")
-  .attr("opacity", 0.8)
+    /* Append text to legend. */
+    legend.selectAll("text")
+      .data(["-5%", "-2.5%", "0%", "2.5%", "5%"])
+      .enter()
+      .append("text")
+      .attr("y", 585)
+      .attr("x", (d, i) => {
+        return `${(i) * 71 + 25}`
+      })
+      .text(d => d)
+      .attr("font-size", "0.7rem")
+      .attr("font-family", "sans-serif")
+      .attr("opacity", 0.8)
+    
+    /* Append more text to the legend. */
+    legend.append("text")
+      .attr("transform", "translate(25, 550)")
+      .attr("font-size", "0.7rem")
+      .attr("font-family", "sans-serif")
+      .text("Muutos prosenteissa vuonna 2021 verrattuna vuoteen 2020.")
 
-/* Append more text to the legend. */
-legend.append("text")
-  .attr("transform", "translate(25, 550)")
-  .attr("font-size", "0.7rem")
-  .attr("font-family", "sans-serif")
-  .text("*Muutos prosenteissa verrattuna edelliseen vuoteen.")
+    const municipalities = await loadAndParseData()
 
-/* Define handleZoom. */
-const handleZoom = (e) => {
-  d3.select("g")
-    .attr("transform", e.transform)
-    .call(hideTooltip)
+    map.selectAll("path")
+      .remove()
+
+    map.selectAll("path")
+      .data(municipalities)
+      .enter()
+      .append("path")
+      .attr("id", (d) => d.properties.natcode)
+      .attr("fill", d => colorScale(d.properties.popchangepercent))
+      .attr("class", "municipality")
+      .attr("name", (d) => d.properties.namefin)
+      .attr("d", path)
+      .on("click", showTooltip)
+
+  /* Create economy map. */
+  } else if (selectedmap == "economy") {
+    console.log("eco")
+
+  /* Create healthcare map. */
+  } else if (selectedmap == "healthcare") {
+    console.log("heca")
+
+    const colorScale = d3.scaleLinear()
+      .domain([2400, 6800])
+      .range(["yellow", "violet"])
+
+    legend.selectAll("defs")
+      .remove()
+    
+    const defs = legend.append("defs")
+
+    const linearGradient = defs.append("linearGradient")
+      .attr("id", "linear-gradient")
+      .attr("x1", "0%")
+      .attr("y1", "0%")
+      .attr("x2", "100%")
+      .attr("y2", "0%")
+
+    linearGradient.selectAll("stop")
+      .data(colorScale.range())
+      .enter()
+      .append("stop")
+      .attr("offset", (d, i) => {
+        return i / (colorScale.range().length - 1)
+      })
+      .attr("stop-color", d => d)
+
+    legend.selectAll("rect")
+      .remove()
+
+    legend.append("rect")
+      .attr("width", 300)
+      .attr("height", 10)
+      .style("fill", "url(#linear-gradient)")
+      .attr("transform", "translate(25, 560)")
+
+    legend.selectAll("text")
+      .remove()
+
+    legend.selectAll("text")
+      .data(["2000", "3000", "4000", "5000", "6000", "7000"])
+      .enter()
+      .append("text")
+      .attr("y", 585)
+      .attr("x", (d, i) => {
+        return `${(i) * 55 + 25}`
+      })
+      .text(d => d)
+      .attr("font-size", "0.7rem")
+      .attr("font-family", "sans-serif")
+      .attr("opacity", 0.8)
+
+    legend.append("text")
+      .attr("transform", "translate(25, 550)")
+      .attr("font-size", "0.7rem")
+      .attr("font-family", "sans-serif")
+      .text("Sote kustannukset yhteensä, euroa/kuntalainen.")
+
+    const municipalities = await loadAndParseData()
+
+    map.selectAll("path")
+      .remove()
+
+    map.selectAll("path")
+    .data(municipalities)
+    .enter()
+    .append("path")
+    .attr("id", d => d.properties.natcode)
+    .attr("fill", d => {
+      if (d.properties.healthcarecost != undefined) {
+        return colorScale(d.properties.healthcarecost)
+      } else {
+        return "white"
+      }
+    })
+    .attr("class", "municipality")
+    .attr("name", d => d.properties.namefin)
+    .attr("d", path)
+    .on("click", showTooltip)
+  }
+
+  /* Call hideTooltip() to hide tooltip when map changes. */
+  hideTooltip()
 }
 
-/* Define zoom. */
-const zoom = d3.zoom()
-  .scaleExtent([1, 5])
-  .translateExtent([[0, 0], [width, height]])
-  .on("zoom", handleZoom)
+/* Select tooltip-element. */
+const tooltip = d3.select("#tooltip")
 
-/* Define initZoom. */
-const initZoom = () => {
-  d3.select("svg")
-    .call(zoom)
+/* Create p-element inside tooltip-div. */
+const tooltipNode = document.getElementById("tooltip")
+const tooltipNodeElement = document.createElement("p")
+tooltipNode.appendChild(tooltipNodeElement)
+
+/* Define showTooltip(). */
+function showTooltip(event) {
+  
+  /* Set opacity on all paths. */
+  d3.selectAll("path")
+    .style("opacity", 0.4)
+    .style("stroke-width", "0.25px")
+
+  /* Select path. */
+  const selected = d3.select(this)
+  
+  /* Set opacity on selected path. */
+  selected
+    .style("opacity", 1.0)
+    .style("stroke-width", "1px")
+
+  /* Access data through selected path. */
+  const selectedProperties = selected._groups[0][0].__data__.properties
+
+  /* Get x and y coordinates. */
+  const x = event.clientX
+  const y = event.clientY + window.scrollY
+  
+  /* Set visibility on tooltip to visible. Also position. */
+  tooltip
+    .style("left", d => `${x - 75}px`)
+    .style("top", d => `${y - 100}px`)
+    .style("visibility", "visible")
+    .on("click", hideTooltip)
+
+  /* Set text in tooltip. */
+  if (mapSelector == "population") {
+    tooltipNodeElement.innerHTML = `<b>${selectedProperties.namefin}</b><br>
+      Väestö: ${selectedProperties.popsize}<br>
+      Väestönlisäys: ${selectedProperties.popchange}<br>
+      Muutos: ${(selectedProperties.popchangepercent * 100).toFixed(2)}%`
+  } else if (mapSelector == "economy") {
+    console.log("2")
+  } else if (mapSelector == "healthcare") {
+    tooltipNodeElement.innerHTML = `<b>${selectedProperties.namefin}</b><br>
+    Perusterveydenhoito: ${selectedProperties.basichealthcarecost}<br>
+    Erikoissairaanhoito: ${selectedProperties.specialhealthcarecost}<br>
+    Sote yhteensä: ${selectedProperties.healthcarecost}`
+  }
 }
 
-/* Define createMap(). This could also be done as a Promise.all.
-Which might make the code more consistent. */
-async function createMap(mapnumber) {
+/* Define hideTooltip(). */
+function hideTooltip() {
+
+  /* Set styles on all paths to default. */
+  d3.selectAll("path")
+    .style("opacity", 1)
+    .style("stroke-width", "0.25px")
+
+  /* Set visibility on tooltip to hidden. */
+  tooltip
+    .style("visibility", "hidden")
+}
+
+/* Define loadAndParseData(). */
+async function loadAndParseData() {
+
+  /* Load geojson-file. */
   const data = await d3.json("kunnat-clipped.geojson")
   const municipalities = data.features
 
-  const population = await d3.json("kunnat-vaestonmuutos-2.json")
+  /* Load population-file. */
+  const population = await d3.json("kunnat-vaestonmuutos.json")
 
   /* Create a lookup table. Using reduce() would be fancier. */
   populationTable = {}
@@ -171,25 +334,23 @@ async function createMap(mapnumber) {
     Object.assign(d.properties, populationTable[parseInt(d.properties.natcode)])
   })
   
-  /* Create a lookup table for healthcare. Same as above. */
+  /* Load healthcare-file and create a lookup table for healthcare.
+  Same as above, but shorter. */
   healthcareTable = {}
   const healthcare = await d3.csv("kunnat-terveydenhoito.csv", (data) => {
     const key = data.municipality
-    const value = parseInt(data.healthcareperperson)
-    
+    const value1 = parseInt(data.healthcareperperson)
+    const value2 = parseInt(data.basichealthcareperperson)
+    const value3 = parseInt(data.specialhealthcareperperson)
+  
+    /* Create entries for healthcareTable. */
     healthcareTable[key] = {
-      healthcarecost: value,
+      healthcarecost: value1,
+      basichealthcarecost: value2,
+      specialhealthcarecost: value3,
     }
   })
   
-  const vals = Object.values(healthcareTable).map(d => d.healthcarecost)
-
-  console.log(Math.max(...vals), Math.min(...vals))
-
-  
-
-
-
   /* Assign values in healthcareTable to municipalities. Same as above. */
   municipalities.forEach(d => {
     if (healthcareTable[d.properties.namefin] != undefined) {
@@ -197,124 +358,28 @@ async function createMap(mapnumber) {
     }
   })
 
-
-
-
-
-  /* Draw paths depending on which map the user chooses. */
-  if (mapnumber == 1) {
-    map.selectAll("path")
-      .remove()
-
-    map.selectAll("path")
-      .data(municipalities)
-      .enter()
-      .append("path")
-      .attr("id", (d) => d.properties.natcode)
-      .attr("fill", d => colorScale1(d.properties.popchangepercent))
-      .attr("class", "municipality")
-      .attr("name", (d) => d.properties.namefin)
-      .attr("d", path)
-      .on("click", showTooltip)
-  } else if (mapnumber == 3) {
-    map.selectAll("path")
-      .remove()
-
-    map.selectAll("path")
-      .data(municipalities)
-      .enter()
-      .append("path")
-      .attr("id", d => d.properties.natcode)
-      .attr("fill", d => {
-        if (d.properties.healthcarecost != undefined) {
-          return colorScale3(d.properties.healthcarecost)
-        } else {
-          return "white"
-        }
-      })
-      .attr("class", "municipality")
-      .attr("name", d => d.properties.namefin)
-      .attr("d", path)
-
-  }
+  /* Return the parsed object with all the data. */
+  return municipalities
 }
 
+/* Define zoom. */
+const zoom = d3.zoom()
+  .scaleExtent([1, 5])
+  .translateExtent([[0, 0], [width, height]])
+  .on("zoom", handleZoom)
 
-
-
-
-
-/* Select tooltip-element. */
-const tooltip = d3.select("#tooltip")
-
-/* Create p-element inside tooltip-div. */
-const tooltipNode = document.getElementById("tooltip")
-const tooltipNodeElement = document.createElement("p")
-tooltipNode.appendChild(tooltipNodeElement)
-
-/* Function to show tooltip. */
-function showTooltip(event) {
-  
-  d3.selectAll("path")
-    .style("opacity", 0.4)
-
-  const selected = d3.select(this)
-  selected.style("opacity", 1.0)
-
-  const selectedProperties = selected._groups[0][0].__data__.properties
-
-  const x = event.clientX
-  const y = event.clientY + window.scrollY
-    
-  tooltip
-    .style("left", d => `${x - 75}px`)
-    .style("top", d => `${y - 90}px`)
-    .style("visibility", "visible")
-    .on("click", hideTooltip)
-
-  tooltipNodeElement.innerHTML = `<b>${selectedProperties.namefin}</b><br>
-    Väestönlisäys: ${selectedProperties.popchange}<br>
-    Muutos: ${(selectedProperties.popchangepercent * 100).toFixed(2)}%`
+/* Define handleZoom. */
+function handleZoom (e) {
+  d3.select("g")
+    .attr("transform", e.transform)
+    .call(hideTooltip)
 }
 
-/* Function to hide tooltip. */
-function hideTooltip() {
-
-  d3.selectAll("path")
-    .style("opacity", 1)
-
-  tooltip
-    .style("visibility", "hidden")
+/* Define initZoom. */
+function initZoom () {
+  d3.select("svg")
+    .call(zoom)
 }
-
-/* Dropdown */
-const dropdownNode = document.getElementById("dropdown")
-const dropdownNodeElement = document.createElement("select")
-dropdownNode.appendChild(dropdownNodeElement)
-dropdownNodeElement.innerHTML =
-  "<option value='population'>Väestönkehitys</option><option value='economy'>Talous</option><option value='healthcare'>Terveydenhoitokustannukset</option>"
-
-/* Dropdown event listener. */
-dropdownNodeElement.addEventListener("change", handleSelect)
-
-/* Dropdown event handler. */
-function handleSelect(event) {
-  const selection = event.target.value
-
-  if (selection == "economy") {
-    console.log("ekko")
-  } else if (selection == "healthcare") {
-    mapSelector = 3
-    createMap(mapSelector)
-  } else if (selection == "population") {
-    mapSelector = 1
-    createMap(mapSelector)
-  }
-}
-
-
-
-
 
 /* Run initZoom(). */
 initZoom()
